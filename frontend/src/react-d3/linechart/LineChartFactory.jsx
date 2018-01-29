98317b784a77462a9769b6bd2f109145fe2865c4
jiwoo park <jiwoop.jj@gmail.com>
2018-01-29 16:04:59 +0900

    restructure folder structure

---
+++ frontend/src/react-d3/linechart/LineChartFactory.jsx
@@ -0,0 +1,27 @@
+import * as d3 from "d3";
+
+export default class BarChartFactory {
+  renderChartStatic() {
+    
+  }
+
+  renderChartTransition() {
+    
+  }
+
+  _drawStaticChart(svgElement, chart) {
+    
+  }
+
+  _applyTransition(g, that, chart) {
+    
+  }
+
+  _prepareChart(chart) {
+    
+  }
+
+  _applyFocus(rect, chart) {
+      
+  }
+}
