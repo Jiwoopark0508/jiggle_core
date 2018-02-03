b99ff59074c26c5e09d29f11cf50c6387eeb77e4
Jake Park <jake@danbi-mac.local>
2018-02-03 14:20:08 +0900

    gif reset

--- frontend/src/react-d3/factory/bar-factory.js
+++ frontend/src/react-d3/factory/bar-factory.js
@@ -2,7 +2,6 @@ import * as d3 from "d3";
 import makeGIF from "../../gif/makeGIF";
 import recordTransition from "../../gif/d3-record";
 
-// branch: gif
 export default class BarFactory {
   renderChart() {
     const renderer = (svgElement, chart) => {
@@ -14,271 +13,15 @@ export default class BarFactory {
   renderTransition() {
     const renderer = (svgElement, charts, shouldMakeGIF = false) => {
       let g = this._drawChart(svgElement, charts[0]);
-      let tweeners = [];
-      let gif = new window.GIF({
-        workers: 1,
-        quality: 10,
-        repeat: 0
-      });
-      let t = [];
       charts.forEach((cht, i) => {
         if (i !== 0) {
           cht.accumedDelay =
             cht.delay + charts[i - 1].duration + charts[i - 1].accumedDelay;
           g.call(this._applyTransition, this, cht);
-          // this._recordTransition(svgElement, cht, g, frames, true);
-          // if (i === 2) console.log(g.selectAll("*"));
-          // t.push(
-          //   this._recordTransition(
-          //     svgElement,
-          //     charts,
-          //     g,
-          //     gif,
-          //     // frames,
-          //     true
-          //   )
-          // ); // [{trans:[], tweeners:[]}, {trans:[], tweeners:[]}]
         }
       });
-      // const allElements = g.selectAll("*");
-
-      // allElements.interrupt();
-      // console.log(t);
-      // t.forEach(function(t1) {
-      //   t1.forEach(function(t2) {
-      //     t2(800);
-      //   });
-      // });
-
-      // const totalDuration =
-      //   charts[charts.length - 1].accumedDelay +
-      //   charts[charts.length - 1].duration;
-      // const frames = 20 * totalDuration / 1000;
-      // let promises = [];
-
-      // promises.push(
-      //   new Promise((resolve0, reject) => {
-      //     this._recordTransition(
-      //       svgElement,
-      //       charts,
-      //       g,
-      //       gif,
-      //       frames,
-      //       true,
-      //       resolve0
-      //     );
-      //   })
-      // );
-
-      // Promise.all(promises).then(function(results) {
-      //   // console.log(results);
-      //   // console.log(results[0] === results[1]);
-      //   results[0].render();
-      // });
-      // gif.on("progress", function(p) {
-      //   // drawFrame(p * totalDuration);
-      //   // d3.select("#gif").text(d3.format("%")(p) + " rendered");
-      //   gifToPresent.text(d3.format("%")(p) + " rendered");
-      // });
-
-      // gif.on("finished", function(blob) {
-      //   gifToPresent
-      //     .text("")
-      //     .append("img")
-      //     .attr("src", URL.createObjectURL(blob));
-
-      // d3.timer(drawFrame);
-      // });
     };
     return renderer;
-  }
-
-  _recordTransition(
-    svgElement,
-    charts,
-    selection,
-    gif,
-    // frames,
-    realtime
-    // resolve0
-  ) {
-    const tweeners = [];
-    const allElements = selection.selectAll("*");
-
-    // console.log(allChildren);
-    allElements.each(function(d, i) {
-      // console.log(this);
-      const node = this;
-      const pending = d3
-        .entries(this.__transition)
-        .filter(function(tr) {
-          return tr.key !== "active" && tr.key !== "count";
-        })
-        .map(function(tr) {
-          return tr.value;
-        });
-
-      if (pending.length === 0) return;
-      // console.log(pending);
-      const cumulativeSum = [pending[0].delay];
-      for (var j = 1; j < pending.length; j++) {
-        cumulativeSum[j] =
-          cumulativeSum[j - 1] + pending[j].delay + pending[j].duration;
-      }
-
-      pending.forEach(function(tran, i) {
-        if (tran.tween.length === 0) return;
-        // console.log(tran.tween);
-        // Probably unnecessary?
-        var ease = tran.ease || (d => d);
-        // console.log(transition);
-
-        // console.log(tran);
-        tran.tween.forEach(function(tween) {
-          // Create a tweener with the tween function and the element's datum and index
-          // Tweens with no change return false
-          const tweener = (tween.value.call(node, d, i) || (() => {})).bind(
-            node
-          );
-
-          // Function that calls the tweener with an eased time value
-          (function(idx) {
-            tweeners.push(function(t) {
-              if (true) {
-                t = relativeTime(t, tran.duration, cumulativeSum[idx]);
-              }
-              tweener(ease(t));
-            });
-          })(i);
-        });
-      });
-    });
-
-    allElements.interrupt();
-
-    // return tweeners;
-
-    // console.log(tweeners);
-    // console.log(allChildren.nodes());
-    // return tweeners;
-
-    // allElements.interrupt();
-    // jumpToTime(1000);
-    // tweeners.forEach(function(tweener) {
-    //   tweener(4800);
-    // });
-
-    // const gifToPresent = d3.select("#gif");
-
-    // // let gif = new window.GIF({
-    // //   workers: 1,
-    // //   quality: 10,
-    // //   repeat: 0
-    // // });
-
-    // const totalDuration =
-    //   charts[charts.length - 1].accumedDelay +
-    //   charts[charts.length - 1].duration;
-    // gif.on("progress", function(p) {
-    //   jumpToTime(p * totalDuration);
-    //   // d3.select("#gif").text(d3.format("%")(p) + " rendered");
-    //   gifToPresent.text(d3.format("%")(p) + " rendered");
-    // });
-
-    // gif.on("finished", function(blob) {
-    //   gifToPresent
-    //     .text("")
-    //     .append("img")
-    //     .attr("src", URL.createObjectURL(blob));
-    //   d3.timer(jumpToTime);
-    // });
-
-    // let promises = [];
-    // d3.range(frames).forEach(function(f) {
-    //   // const t0 = f * chart.duration / (frames - 1) + chart.accumedDelay;
-    //   // console.log(t0);
-    //   promises.push(
-    //     new Promise(function(resolve, reject) {
-    //       addFrame(f / (frames - 1) * totalDuration, resolve);
-    //     })
-    //   );
-    // });
-
-    // console.log(tweeners);
-
-    // Promise.all(promises).then(function(arrOfResults) {
-    //   // svgToRender.style("display", "block");
-    //   // gif.render();
-    //   resolve0(gif);
-    // });
-
-    function relativeTime(ms, duration, delay) {
-      return Math.min(1, Math.max(0, (ms - delay) / duration));
-    }
-
-    // function addFrame(t, resolve) {
-    //   // console.log(svgElement);
-    //   jumpToTime(t);
-    //   var img = new Image(),
-    //     serialized = new XMLSerializer().serializeToString(svgElement),
-    //     // serialized = new XMLSerializer().serializeToString(svg.node()),
-    //     blob = new Blob([serialized], { type: "image/svg+xml" }),
-    //     url = URL.createObjectURL(blob);
-
-    //   img.onload = function() {
-    //     // console.log(chart.duration / frames);
-    //     gif.addFrame(img, {
-    //       delay: totalDuration / frames,
-    //       copy: true
-    //     });
-    //     resolve();
-    //   };
-    //   img.src = url;
-    // }
-
-    function jumpToTime(t) {
-      tweeners.forEach(function(tweener) {
-        tweener(t);
-      });
-    }
-  }
-
-  _makeGIF(chart, jumpToTime, frames) {
-    const gifToPresent = d3.select("#gif");
-
-    let gif = new window.GIF({
-      workers: 1,
-      quality: 10,
-      repeat: 0
-    });
-
-    gif.on("progress", function(p) {
-      jumpToTime(p * chart.duration);
-      // d3.select("#gif").text(d3.format("%")(p) + " rendered");
-      gifToPresent.text(d3.format("%")(p) + " rendered");
-    });
-
-    gif.on("finished", function(blob) {
-      gifToPresent
-        .text("")
-        .append("img")
-        .attr("src", URL.createObjectURL(blob));
-      d3.timer(jumpToTime);
-    });
-
-    let promises = [];
-    d3.range(frames).forEach(function(f) {
-      promises.push(
-        new Promise(function(resolve, reject) {
-          // addFrame(f * chart.duration / (frames - 1), resolve);
-        })
-      );
-    });
-
-    Promise.all(promises).then(function(arrOfResults) {
-      // svgToRender.style("display", "block");
-      gif.render();
-    });
   }
 
   _drawChart(svgElement, chart) {
@@ -313,35 +56,22 @@ export default class BarFactory {
   }
 
   _applyTransition(g, that, chart) {
-    // const transitions = [];
-    // const tweeners = [];
-
     g
       .select(".x.axis")
       .transition()
       .duration(chart.duration)
       .delay(chart.accumedDelay)
       .call(d3.axisBottom(chart.xScale));
-    // .call(that._recordTransition, transitions, tweeners, true);
     // Update selection
     let rect = g.selectAll("rect").data(chart.data, chart.dataKey);
 
-    // console.log(rect);
-    // let groupedG = g.selectAll("rect");
-    // const jumpToTime = recordTransition(rect, true);
-
-    let removingRect = rect
+    rect
       .exit() // Exit selection
       .transition()
       .duration(chart.duration / 2)
       .delay(chart.accumedDelay)
       .style("opacity", 0)
-      // .call(that._recordTransition, transitions, tweeners, true)
       .remove();
-
-    // console.log(removingRect);
-    // const jumpToTime = recordTransition(removingRect, true);
-    // removingRect.remove();
 
     rect
       .enter() // Enter selection
@@ -358,12 +88,6 @@ export default class BarFactory {
       .attr("y", d => chart.yScale(d[chart.yLabel]))
       .attr("width", chart.xScale.bandwidth())
       .attr("height", d => chart.height_g - chart.yScale(d[chart.yLabel]));
-    // .call(that._recordTransition, transitions, tweeners, true);
-
-    // const jumpToTime = recordTransition(rect, true);
-    // jumpToTime(800);
-    // console.log(tweeners);
-    // removingRect.selection().interrupt();
   }
 
   _applyFocus(rect, chart) {
